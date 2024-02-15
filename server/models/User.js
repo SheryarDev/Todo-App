d2c20dc34e2247e7be37260492c1d3c847e59f30
sheryarDev <sheryarking5@gmail.com>
2024-02-15 20:43:30 +0500

    inital commit

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+  
+  },
+  { timestamps: true }
+);
+
+
+
+export default mongoose.model("User", userSchema);
